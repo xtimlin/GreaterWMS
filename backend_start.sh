29aaa0b91c7718243e14e483bc1f43309b1275a0
xtimxlin <xtimxlin@gmail.com>
2025-02-27 22:16:44 -0500

    local push

--- backend_start.sh
+++ backend_start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-# Run database migrations
+python3 manage.py makemigrations
 python3 manage.py migrate
-
-# Start the Django development server
-python3 manage.py runserver 0.0.0.0:8000
+supervisord -c /etc/supervisor/supervisord.conf
